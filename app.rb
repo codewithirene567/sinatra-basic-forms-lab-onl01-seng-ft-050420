102f56514520fa41d278531aa256fe3d56623947
Irene Scott <irenejinscott@gmail.com>
2020-06-19 06:06:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ end
 
 post '/puppy' do
   binding.pry
-  @puppy = Puppy.new.initialize(params[:name, params:breed, params:age])
+  @puppy = Puppy.new(params[:name, params:breed, params:age])
 
   erb :display_puppy
 end
