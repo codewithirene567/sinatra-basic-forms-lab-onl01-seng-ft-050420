c9b12677df7407bfb240da95cb28d132ba1ed646
Irene Scott <irenejinscott@gmail.com>
2020-06-19 05:58:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ get '/new' do
 end
 
 post '/puppy' do
-  @puppy = Puppy.new.initialize(name, breed, age)(params[])
+  @puppy = Puppy.new.initialize(name, breed, age)(params[:puppy])
   erb :display_puppy
 end
 
